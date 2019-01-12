ec598327e041f6f7a5bb805d7221e9222e8dd0bc
Rick Buitrago <rbuitragoc@gmail.com>
2019-01-12 16:16:23 -0500

    1.4 version: boxen header, added mobile

--- build.js
+++ build.js
@@ -7,35 +7,46 @@ const fs = require('fs')
 const path = require('path')
 
 // Define options for Boxen
-const options = {
+const outerBoxenOptions = {
   padding: 1,
   margin: 1,
   borderStyle: 'round'
 }
 
+const headerBoxenOptions = {
+  padding: 0, 
+  margin: 0,
+  borderStyle: 'double', 
+  borderColor: 'grey'
+}
+
 // Text + chalk definitions
 const data = {
-  name: chalk.white('            Rick Buitrago'),
+  name: chalk.white(' Rick Buitrago'),
   handle: chalk.white('deekattax'),
+  bio: chalk.gray('Scaling Java/JS apps, Team Building with Agile '),
   work: chalk.white('\u2615 Lead Developer at Endava'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('deekattax'),
   github: chalk.gray('https://github.com/') + chalk.green('rbuitragoc'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('rbuitrago  '),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('rbuitrago'),
+  mobile: chalk.gray('+57 ') + chalk.yellow('300 2691858'),
   npx: chalk.red('npx') + ' ' + chalk.white('deekattax'),
   labelWork: chalk.white.bold('       Work:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelMobile: chalk.white.bold('     Mobile:'),
   labelCard: chalk.white.bold('       Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} / ${data.handle}`
+const heading = boxen(`${data.name} (${data.handle}) ${newline} ${data.bio}`, headerBoxenOptions)
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const mobileing = `${data.labelMobile}  ${data.mobile}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
@@ -44,7 +55,8 @@ const output = heading + // data.name + data.handle
                working + newline + // data.labelWork + data.work
                twittering + newline + // data.labelTwitter + data.twitter
                githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + newline + // data.labelLinkedIn + data.linkedin
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               mobileing + newline + newline + // data.labelMobile + data.mobile
                carding // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, outerBoxenOptions)))
