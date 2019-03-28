c91834dd596642c2260a924996069d2366c41565
Rick Buitrago <rbuitragoc@gmail.com>
2019-03-27 19:31:16 -0500

    updating tag line

--- build.js
+++ build.js
@@ -24,19 +24,19 @@ const headerBoxenOptions = {
 const data = {
   name: chalk.white(' Rick Buitrago'),
   handle: chalk.white('deekattax'),
-  bio: chalk.gray('Scaling Java/JS apps, Team Building with Agile '),
+  bio: chalk.gray('Scaling apps with Java/JS, Building Teams with Agile '),
   work: chalk.white('\u2615 Senior Software Engineer'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('deekattax'),
   github: chalk.gray('https://github.com/') + chalk.green('rbuitragoc'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('rbuitrago'),
   mobile: chalk.gray('+57 ') + chalk.yellow('300 2691858'),
   npx: chalk.red('npx') + ' ' + chalk.white('deekattax'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelMobile: chalk.white.bold('     Mobile:'),
-  labelCard: chalk.white.bold('       Card:')
+  labelWork: chalk.white.bold('         Work:'),
+  labelTwitter: chalk.white.bold('      Twitter:'),
+  labelGitHub: chalk.white.bold('       GitHub:'),
+  labelLinkedIn: chalk.white.bold('     LinkedIn:'),
+  labelMobile: chalk.white.bold('       Mobile:'),
+  labelCard: chalk.white.bold('         Card:')
 }
 
 // Actual strings we're going to output
